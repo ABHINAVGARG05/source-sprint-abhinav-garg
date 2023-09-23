4a6056d3805348aec2da038480626fa9cc9d6f8c
ABHINAVGARG05 <143117260+ABHINAVGARG05@users.noreply.github.com>
2023-09-23 14:48:49 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+x = int(input("Enter the first number: "))
+print("the list of operators is: + - * /")
+a=input("Please select your operator : ")
+y = int(input("Enter the second number: "))
+if a == "+" :
+    print(x+y)
+if a == "-"  :
+    print(x-y)
+if a == "*" :
+    print(x*y)
+if a == "/" :
+    print(x/y)
+if a!="+" and a!="-" and a!="*" and a!="/" :
+    print("YOUR ARE AN IDIOT")
