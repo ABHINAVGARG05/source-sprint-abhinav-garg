ad22efa6641dad709effcf40e137c291d64dbade
ABHINAVGARG05 <143117260+ABHINAVGARG05@users.noreply.github.com>
2023-09-23 14:54:26 +0530

    new feature added

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 
 x = int(input("Enter the first number: "))
-print("the list of operators is: + - * /")
+print("the list of operators is: + - * / %")
 a=input("Please select your operator : ")
 y = int(input("Enter the second number: "))
 if a == "+" :
@@ -11,5 +11,7 @@ if a == "*" :
     print(x*y)
 if a == "/" :
     print(x/y)
+if a == "%":
+    print(x%y)
 if a!="+" and a!="-" and a!="*" and a!="/" :
     print("YOUR ARE AN IDIOT")
